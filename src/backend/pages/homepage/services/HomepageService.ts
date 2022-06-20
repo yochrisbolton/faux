0337db273044ce348b04d96b3c7ce574bf837741
Chris Bolton <hello@chrisbolton.dev>
2022-06-19 22:56:11 -0400

    Update env var names

--- src/backend/pages/homepage/services/HomepageService.ts
+++ src/backend/pages/homepage/services/HomepageService.ts
@@ -3,8 +3,8 @@ import { Request, Response } from 'express'
 export class HomepageService {
   public async render (_req: Request, res: Response): Promise<void> {
     const pageBanner = {
-      title: process.env.PROJECT_NAME,
-      info: process.env.COMMON_DESC
+      title: process.env.PROJECT_NAME ?? 'faux',
+      info: process.env.COMMON_DESCRIPTION ?? 'batteries included TypeScript Express starter'
     }
 
     return res.render('templates/pages/homepage/index', { pageBanner: pageBanner })
